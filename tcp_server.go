1c17ff02dac69d0799c26ef9f08e69c289853dd4
thevithach <112381637+thevithach@users.noreply.github.com>
2023-04-24 09:16:59 +0200

    Add files via upload
    push

---
+++ tcp_server.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"sync"
+	"github.com/thevithach/is105sem03/mycrypt"
+)
+
+func main() {
+	
+	var wg sync.WaitGroup
+
+	server, err := net.Listen("tcp", "172.17.0.4:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("bundet til %s", server.Addr().String())
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			log.Println("før server.Accept() kallet")
+			conn, err := server.Accept()
+			if err != nil {
+				return
+			}
+			go func(c net.Conn) {
+				defer c.Close()
+				for {
+					buf := make([]byte, 1024)
+					n, err := c.Read(buf)
+					if err != nil {
+						if err != io.EOF {
+							log.Println(err)
+						}
+						return // fra for løkke
+					}
+					dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+					log.Println("Dekrypter melding: ", string(dekryptertMelding))
+					switch msg := string(dekryptertMelding); msg  {
+  				        case "ping":
+						_, err = c.Write([]byte("pong"))
+					default:
+						_, err = c.Write(buf[:n])
+					}
+					if err != nil {
+						if err != io.EOF {
+							log.Println(err)
+						}
+						return // fra for løkke
+					}
+				}
+			}(conn)
+		}
+	}()
+	wg.Wait()
+}
